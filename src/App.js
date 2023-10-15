f492d3aa1005efec63a77b16462f5e2ce5d7d866
Ahmed Walid <144860846+AhmedWalid100@users.noreply.github.com>
2023-10-15 08:56:37 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Navbar from './components/Navbar.js';
 import Home from './components/Home';
 import Portfolio from './components/Portfolio.js';
+import About from './components/About';
 function App() {
   return (
     <>
     <Navbar/>
     <Home/>
     <Portfolio/>
-      
+    <About/>  
     </>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './About.css'
+function About() {
+  return (
+
+    <div className='aboutContainer'>
+        <h2>ABOUT ME</h2>
+        <div className='aboutSmallContainer'>
+            <div><h3>Get to know me!</h3><p>Hi, I'm Ahmed Walid, a front-end developer with 1 year of experience.
+                 I graduated from AAST with a Bachelor of Science degree in Computer Engineering.
+My skills in front-end development include HTML, CSS, JavaScript, React, Node.js, and WordPress.<br/></p>
+<p>I also know other languages, as I have knowledge in game development and other computer science fields like,
+ computer networks, machine learning and data acquisition. (C#, Python, Java, C, MySQL) </p>
+However, my focus right now is on Front-End Development. The projects and skills I will be displaying on my website 
+will be focused towards Front-End Development.
+I'm passionate about creating beautiful, user-friendly web applications.
+ I'm excited to use my skills and experience. Please feel free to contact me if you have any questions.</div>
+            <div><h3>My Skills</h3><div className='skills'><h5>HTML</h5>
+<h5>CSS</h5>
+<h5>JavaScript</h5>
+<h5>React</h5>
+<h5>Node.js</h5>
+<h5>Express.js basics</h5>
+<h5>MySQL</h5>
+<h5>Unity</h5>
+<h5>.NET</h5>
+<h5>C#</h5>
+<h5>Python</h5>
+<h5>Java</h5>
+<h5>OOP</h5>
+<h5>Algorithms</h5>
+<h5>Data Structure</h5></div></div>
+        </div>
+    </div>
+  )
+}
+
+export default About
